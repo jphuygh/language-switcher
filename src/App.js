4cee1f57f09772a4e835915200418d4dbc017c0a
jhpullrequest <jeff@pullrequest.com>
2021-05-05 22:17:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import translations from './translations';
+
+function Selector(props) {
+  const { lang } = props;
+
+  const languages = {
+    'en': 'English',
+    'fr': 'French',
+    'de': 'German',
+    'it': 'Italian',
+  };
+
+  const onChangeHandler = (event) => {
+    const { setLang } = props;
+    setLang(event.target.value);
+  }
+
+  return (
+    <select onChange={onChangeHandler} defaultValue={lang}>
+      {Object.keys(languages).map((key) => (
+        <option key={key} value={key}>{languages[key]}</option>
+      ))}
+    </select>
+  )
+}
+
+const Heading = (props) => {
+  const { lang } = props;
+  const strings = translations[lang];
+  return (
+    <h1>{strings['heading']}</h1>
+  );
+}
+
+const Body = (props) => {
+  const { lang } = props;
+  const strings = translations[lang];
+  return (
+    <p className="body">{strings['body']}</p>
+  );
+}
+
+const Footer = (props) => {
+  const { lang } = props;
+  const strings = translations[lang];
+  return (
+    <p className="footer">{strings['footer']}</p>
+  );
+}
 
 function App() {
+  const [lang, setLang] = useState('en');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Selector setLang={setLang} />
+      <Heading lang={lang} />
+      <Body lang={lang} />
+      <Footer lang={lang} />
     </div>
   );
 }
